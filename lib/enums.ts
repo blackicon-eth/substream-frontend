e6ed5e7b6d8829d35e026d0c042c15d19bfb7a43
Blackicon <mattiaverdecchi@yahoo.it>
2025-07-05 17:07:52 +0200

    Added reown appkit

---
+++ lib/enums.ts
@@ -0,0 +1,5 @@
+// Alchemy RPC Base URLs
+export enum AlchemyRpcBaseUrls {
+  "ethereum" = "https://eth-mainnet.g.alchemy.com/v2",
+  "sepolia" = "https://eth-sepolia.g.alchemy.com/v2",
+}
--- lib/env.ts
+++ lib/env.ts
@@ -7,9 +7,17 @@ export const env = createEnv({
   client: {
     // NEXT.JS
     NEXT_PUBLIC_URL: z.string().min(1),
+
+    // REOWN APP ID
+    NEXT_PUBLIC_REOWN_APP_ID: z.string().min(1),
+
+    // ALCHEMY API KEY
+    NEXT_PUBLIC_ALCHEMY_API_KEY: z.string().min(1),
   },
   // For Next.js >= 13.4.4, you only need to destructure client variables:
   experimental__runtimeEnv: {
     NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+    NEXT_PUBLIC_REOWN_APP_ID: process.env.NEXT_PUBLIC_REOWN_APP_ID,
+    NEXT_PUBLIC_ALCHEMY_API_KEY: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
   },
 });
